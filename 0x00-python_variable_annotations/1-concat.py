9293318ddecddb6ad911fea2a1e9158413062b44
ahmedhalimx <ahmedhalimcs101@gmail.com>
2024-01-25 18:32:31 +0200

    forgot 0x00-python_variable_annotations folder

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+
+"""
+    1-concat.py
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """
+    Returns the concatination of two strings
+    """
+    return str1 + str2
