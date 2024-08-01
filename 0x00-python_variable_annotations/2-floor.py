1307335d70cfdc660e168855332dfc2c55626d70
ahmedhalimx <ahmedhalimcs101@gmail.com>
2024-08-01 18:57:55 +0300

    yet another application

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+'''
+    defines floor() function
+'''
+
+import math
+
+
+def floor(n: float) -> int:
+    '''
+        returns the floor of n
+    '''
+    return math.floor(n)
