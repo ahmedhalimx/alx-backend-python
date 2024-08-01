e1b4be7b10a8928f76b8d04f17d19c22b3381384
ahmedhalimx <ahmedhalimcs101@gmail.com>
2024-08-01 19:38:00 +0300

    apperantly i was right, its just alx uses python 3.7 instead of 3.10 or higher, which caused an error when using List[x|y] syntax

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -4,6 +4,6 @@
 from typing import List, Union
 
 
-def sum_mixed_list(mxd_lst: List[Union[int | float]]) -> float:
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
     """returns the sum of elements in mxd_lst"""
     return sum(mxd_lst)
